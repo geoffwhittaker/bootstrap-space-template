ed41522c0a4f7b87a435a310bb8fedf3ac82f920
geoffrey Whittaker <gwhittaker@uclan.ac.uk>
2018-03-25 13:18:54 +0100

    First commit

---
+++ index.php
@@ -0,0 +1,134 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>Bootstrap Space theme</title>
+  <meta name="description" content="Space theme">
+  <meta name="author" content="Geoffrey Whittaker">
+
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/styles.css">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="scripts/bootstrap.min.js"></script>
+<script src="scripts/bootstrap.tab.js"></script>
+
+  <!--[if lt IE 9]>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
+  <![endif]-->
+</head>
+
+<body>
+<div class="container">
+     <header class="row">
+        <div class="col-lg-12">
+        <nav class="navbar navbar-expand-lg navbar">
+          <a class="navbar-brand" href="#">Space news</a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"><i class="material-icons" style="font-size:30px">menu</i></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+              <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+              <a class="nav-item nav-link" href="#">Features</a>
+              <a class="nav-item nav-link" href="#">Pricing</a>
+              <a class="nav-item nav-link" href="#">Gallery</a>
+              <a class="nav-item nav-link" href="#">Slide show</a>
+            </div>
+          </div>
+        </nav>
+     </div>
+    </header>
+    <div class="row">
+        <div class="col-lg-8">
+        <p>
+        
+       <article id="mars"> 
+         <h3>Mars missions</h3>
+        
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+          Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+          when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+            It has survived not only five centuries, but also the leap into electronic typesetting, 
+            remaining essentially unchanged. It was popularised in the 1960s with the release of 
+            Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing 
+            software like Aldus PageMaker including versions of Lorem Ipsum.
+       </article>
+
+            <nav class="tabs">
+          <div class="nav nav-tabs" id="nav-tab" role="tablist">
+            <a class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true"> <i class="material-icons" style="font-size:15px">call</i>Planet</a>
+            <a class="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="false"><i class="material-icons" style="font-size:15px">star_border</i>Interior</a>
+            <a class="nav-item nav-link" id="nav-contact-tab" data-toggle="tab" href="#nav-contact" role="tab" aria-controls="nav-contact" aria-selected="false"><i class="material-icons" style="font-size:15px">fiber_smart_record</i>Missions</a>
+          </div>
+        </nav>
+        <div class="tab-content" id="nav-tabContent">
+          <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
+          <h3>Tabs</h3>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+            when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+            It has survived not only five centuries, but also the leap into electronic typesetting, 
+            remaining essentially unchanged. It was popularised in the 1960s with the release of 
+            Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing 
+            software like Aldus PageMaker including versions of Lorem Ipsum.
+          </div>
+          <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+            when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+            It has survived not only five centuries, but also the leap into electronic typesetting, 
+            remaining essentially unchanged. It was popularised in the 1960s with the release of 
+            Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing 
+            software like Aldus PageMaker including versions of Lorem Ipsum.
+          </div>
+          <div class="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+            when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+            It has survived not only five centuries, but also the leap into electronic typesetting, 
+            remaining essentially unchanged. It was popularised in the 1960s with the release of 
+            Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing 
+            software like Aldus PageMaker including versions of Lorem Ipsum.
+          </div>
+        </div>
+
+          Why do we use it?
+          It is a long established fact that a reader will be distracted by the readable 
+          content of a page when looking at its layout. The point of using Lorem Ipsum is 
+          that it has a more-or-less normal distribution of letters, as opposed to using 
+          'Content here, content here', making it look like readable English. Many desktop 
+          publishing packages and web page editors now use Lorem Ipsum as their default model text, 
+          and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various 
+          versions have evolved over the years, sometimes by accident, sometimes on purpose (injected
+          humour and the like).
+
+     
+        </div>
+        <div class="col-lg-4">
+        <h3> Sidebar </h3>
+        <p>
+          Why do we use it?
+          It is a long established fact that a reader will be distracted by the readable 
+          content of a page when looking at its layout. The point of using Lorem Ipsum is 
+          that it has a more-or-less normal distribution of letters, as opposed to using 
+          'Content here, content here', making it look like readable English. Many desktop 
+          publishing packages and web page editors now use Lorem Ipsum as their default model text, 
+          and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various 
+          versions have evolved over the years, sometimes by accident, sometimes on purpose (injected
+          humour and the like).</p>
+        </div>
+    </div>
+    <footer class="row">
+    <div class="col-lg-12">
+    <p>Footer</p>
+    </div>
+    </footer>
+</div>
+  <script src="js/scripts.js"></script>
+</body>
+</html>
